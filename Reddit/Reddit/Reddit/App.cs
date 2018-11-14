9b49b2550c9677335d4584723a704025ee734945
Ebubekir <32433476+EbuSezer@users.noreply.github.com>
2018-11-14 20:22:59 +0300

    inital created

---
+++ Reddit/Reddit/Reddit/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Reddit
+{
+   public partial class App:Application
+    {
+        public App()
+        {
+            NavigationPage page = new NavigationPage(new MainPage());
+            page.BackgroundColor = Color.White;
+            page.BarBackgroundColor = Color.White;
+            MainPage = page;
+        }
+    }
+}
---
+++ Reddit/Reddit/Reddit/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Reddit
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+            FillInfo();
+		}
+        private void FillInfo()
+        {
+            List<model> models = new List<model>
+            {
+                new model
+                {
+                    name="1"
+                },
+                 new model
+                {
+                    name="1"
+                },
+                 new model
+                {
+                    name="1"
+                },
+                 new model
+                {
+                    name="1"
+                },
+                 new model
+                {
+                    name="1"
+                },
+                 new model
+                {
+                    name="1"
+                },
+                 new model
+                {
+                    name="1"
+                }
+            };
+            listPost.BindingContext = models;
+        }
+        class model
+        {
+            public string name { get; set; }
+        }
+        private void buttonHide_Clicked(object sender, EventArgs e)
+        {
+            stack.IsVisible = false;
+        }
+
+        
+    }
+}
